9c5ca5ed53156976b821e9f0fb1b9e453cdc71b1
Petr <p.e.shirin@gmail.com>
2020-11-18 14:58:47 +0800

    add AboutAs component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import './App.css'
 import SiteHeader from "./components/siteHeader/siteHeader"
+import AboutAs from "./components/AboutAs/AboutAs";
 
 function App() {
   return (
     <div className="App">
       <SiteHeader />
+      <AboutAs />
     </div>
   );
 }
---
+++ src/components/AboutAs/AboutAs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './AboutAs.module.css'
+
+const AboutAs = () => (
+    <div className={styles.container}>
+        <div className={styles.header}>О нас</div>
+        <div className={styles.contentContainer}>
+        <div className={styles.textContainer}>
+            <p>Lapkins — это место, где каждый четвероногий друг получит достойную стрижку и уход от профессионалов.</p>
+            <p>Мы — творческая команда, которая бесконечно любит животных и считает груминг своим призванием. Вы можете доверять нам</p>
+        </div>
+        <div className={styles.imgContainer}>
+
+        </div>
+        </div>
+    </div>
+)
+
+export default AboutAs
+
+
